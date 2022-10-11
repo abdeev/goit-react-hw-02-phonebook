b5169c9f8fe84a555c450278adfd2acf1343937b
Abdeev Vladimir <107181781+abdeev@users.noreply.github.com>
2022-10-11 21:00:26 +0300

    prettified files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,49 +4,48 @@ import Filter from '../Filter/Filter';
 import ContactsList from '../ContactsList/ContactsList';
 import css from './App.module.css';
 
-
 export default class App extends Component {
   state = {
-        contacts: [
-            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-        ],
-        name: '',
-        number: '',
-        filter: ''
-    }
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
 
-    formSubmitHandler = (data) => {
-        if (this.state.contacts.some(({ name }) => name === data.name)) {alert(`${data.name} is already in contacts!`);
+  formSubmitHandler = data => {
+    if (this.state.contacts.some(({ name }) => name === data.name)) {
+      alert(`${data.name} is already in contacts!`);
       return;
     }
-        this.setState(prev => ({ contacts: [...prev.contacts, data] }));
-    };
-    handleFilter = e => {this.setState({ filter: e.currentTarget.value });
-    };
-    handleDeleteContact = id => {
-     this.setState(prev => ({
+    this.setState(prev => ({ contacts: [...prev.contacts, data] }));
+  };
+  handleFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+  handleDeleteContact = id => {
+    this.setState(prev => ({
       contacts: prev.contacts.filter(contact => contact.id !== id),
     }));
   };
-    render() {
-        const { contacts, filter } = this.state;
-        const filteredContacts = contacts.filter(({ name }) => name.toLowerCase().includes(filter.toLowerCase()));
-      return (
-        <div className={css.section}>
-          <h1 className={css.header}>Phonebook</h1>
-          <ContactForm submitProps={this.formSubmitHandler} />
-          <h2 className={css.header}>Contacts</h2>
-          <Filter
-                    filter={filter}
-                    onFilter={this.handleFilter}
-          />
-          <ContactsList
-                    contacts={filteredContacts}
-                    handleDeleteCard={this.handleDeleteContact}
-          />
-        </div>
-      )}
-};
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+    return (
+      <div className={css.section}>
+        <h1 className={css.header}>Phonebook</h1>
+        <ContactForm submitProps={this.formSubmitHandler} />
+        <h2 className={css.header}>Contacts</h2>
+        <Filter filter={filter} onFilter={this.handleFilter} />
+        <ContactsList
+          contacts={filteredContacts}
+          handleDeleteCard={this.handleDeleteContact}
+        />
+      </div>
+    );
+  }
+}
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,11 @@ import css from './Button.module.css';
 
 const DeleteButton = ({ handleDeleteCard }) => {
   return (
-    <button type="button" onClick={handleDeleteCard} className={css.delete_button}>
+    <button
+      type="button"
+      onClick={handleDeleteCard}
+      className={css.delete_button}
+    >
       Delete
     </button>
   );
